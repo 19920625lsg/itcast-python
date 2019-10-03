d4d9c0584b57c75ae25ac950c6215bb049cdac22
liang shan guang <1648266192@qq.com>
2019-10-03 18:25:40 +0800

    使用list_display来控制显示哪些字段

--- Django2入门与实践/mysite/polls/admin.py
+++ Django2入门与实践/mysite/polls/admin.py
@@ -13,6 +13,8 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    # 配置要展示的字段
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     # 对字段进行分组
     fieldsets = [
         (None, {'fields': ['question_text']}),
