15c0deefe592d9ec911eb925dee0e7c337e8d1b5
liang shan guang <1648266192@qq.com>
2019-10-03 18:40:00 +0800

    自定义admin的样式和标题 import django print(django.__path__)找到django包的路径，然后拷贝出base_site.html进行修改一般路径如下 python3.6/site-packages/django/contrib/admin/templates/admin/base_site.html

--- Django2入门与实践/mysite/mysite/settings.py
+++ Django2入门与实践/mysite/mysite/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '!t2y0ct#=9n-v^(5c-2f%suq^=3-*r-=z-kfg(__mwx$j+p_fk'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -55,7 +53,9 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 添加自定义的系统路径
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
@@ -81,10 +80,9 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': 'aA111111',
         'HOST': '119.3.80.210',
-        'PORT': 3306 
+        'PORT': 3306
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -104,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -117,7 +114,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
--- Django2入门与实践/mysite/polls/admin.py
+++ Django2入门与实践/mysite/polls/admin.py
@@ -15,6 +15,10 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     # 配置要展示的字段
     list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # 设置可以进行过滤筛选的字段
+    list_filter = ['pub_date']
+    # 设置可搜索字段
+    search_fields = ['question_text']
     # 对字段进行分组
     fieldsets = [
         (None, {'fields': ['question_text']}),
