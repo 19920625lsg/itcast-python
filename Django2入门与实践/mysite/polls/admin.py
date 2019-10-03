3faaa18db760bd1f5b1dd46b9b7ff920ec8715f5
liang shan guang <1648266192@qq.com>
2019-10-03 18:21:23 +0800

    实现了问题和选项的二合一录入

--- Django2入门与实践/mysite/polls/admin.py
+++ Django2入门与实践/mysite/polls/admin.py
@@ -3,13 +3,23 @@ from .models import Question, Choice
 
 
 # Register your models here.
+
+class ChoiceInline(admin.StackedInline):
+    """
+    外键关联的类在界面上放到一起
+    """
+    model = Choice
+    extra = 2
+
+
 class QuestionAdmin(admin.ModelAdmin):
     # 对字段进行分组
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']})
     ]
+    inlines = [ChoiceInline]
 
 
+# 问题和选项录入二合一
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
